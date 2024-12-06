115e05cbe80e7c39d4ffc32bc095c9eed875ba4f
RicardoRios <72361340+RicardoRios07@users.noreply.github.com>
2024-12-05 19:22:06 -0500

    Update routing in App component and configure server port in Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PokemonList from './components/PokemonList';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Pokémon Api</h1>
-        <Switch>
-          <Route path="/" component={PokemonList} />
-
-        </Switch>
+        <h1>Pokémon API</h1>
+        <Routes>
+          <Route path="/" element={<PokemonList />} />
+        </Routes>
       </div>
     </Router>
   );
